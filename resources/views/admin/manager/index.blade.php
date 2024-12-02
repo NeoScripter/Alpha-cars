b893f2ce77981a5cdfe5063127de9e0b5c86cbad
Ilya <sange0337@gmail.com>
2024-12-02 21:32:08 +0800

    create a search form component

--- resources/views/admin/manager/index.blade.php
+++ resources/views/admin/manager/index.blade.php
@@ -3,25 +3,7 @@
         <h2 class="flex flex-wrap items-center justify-between gap-4 text-lg font-medium text-gray-900">
             Все менеджеры
 
-            <form class="max-w-sm sm:ml-auto w-100 shrink-0" method="GET"
-                onsubmit="event.preventDefault(); window.location.href = '/admin/managers/' + encodeURIComponent(this.search.value);">
-                <label for="default-search"
-                    class="text-sm font-medium text-gray-900 sr-only dark:text-white">Поиск</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" name="search" id="default-search"
-                        class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-gray-900 focus:border-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Найти менеджера..." />
-                    <button type="submit"
-                        class="text-white absolute end-2.5 bottom-2.5 bg-gray-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Поиск</button>
-                </div>
-            </form>
+            <x-admin.search-form :action="'/admin/managers/'" :placeholder="'Найти менеджера...'" />
         </h2>
     </div>
 
@@ -47,7 +29,7 @@
                         @if ($manager->image)
                             <div>
                                 <figure class="relative max-w-sm mb-1">
-                                    <img class="rounded-lg" src="{{ Storage::url($manager->image) }}"
+                                    <img class="rounded-lg max-w-32" src="{{ Storage::url($manager->image) }}"
                                         alt="Фото менеджера">
                                 </figure>
                             </div>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,25 +3,7 @@
         <h2 class="flex flex-wrap items-center justify-between gap-4 text-lg font-medium text-gray-900">
             Все пользователи
 
-            <form class="max-w-sm sm:ml-auto w-100 shrink-0" method="GET"
-                onsubmit="event.preventDefault(); window.location.href = '/admin/users/' + encodeURIComponent(this.search.value);">
-                <label for="default-search"
-                    class="text-sm font-medium text-gray-900 sr-only dark:text-white">Поиск</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" name="search" id="default-search"
-                        class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-gray-900 focus:border-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Найти пользователя..." />
-                    <button type="submit"
-                        class="text-white absolute end-2.5 bottom-2.5 bg-gray-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Поиск</button>
-                </div>
-            </form>
+            <x-admin.search-form :action="'/admin/users/'" :placeholder="'Найти пользователя...'" />
         </h2>
     </div>
 
@@ -44,7 +26,7 @@
                         @isset ($user->image)
                             <div>
                                 <figure class="relative max-w-sm mb-1">
-                                    <img class="rounded-lg" src="{{ Storage::url($user->image) }}"
+                                    <img class="rounded-lg max-w-32" src="{{ Storage::url($user->image) }}"
                                         alt="Фото пользователя">
                                 </figure>
                             </div>
